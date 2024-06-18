3c81b5dfd35e1041f34e185fa2289ccbdadff088
Jack Spencer <UniformKitten281@gmail.com>
2024-06-17 19:48:37 -0700

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,22 @@
+{
+    "Username": "",
+    "Name": "",
+    "Age": null,
+    "Birthday": "",
+    "Gender": "",
+
+    "BotName": "",
+    "NewsAPIKey": "",
+    
+    "Location": {
+        "Country": "",
+        "City": "",
+        "State": ""
+    },
+
+    "ProgrammingLanguages": [],
+
+    "SpotifyClientID": "",
+    "SpotifyClientSecret": "",
+    "SpotifyRedirectURI": ""
+}
